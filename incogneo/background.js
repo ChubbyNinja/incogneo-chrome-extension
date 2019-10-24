6ce4aabf08cb76e70946a0cbb27a2076d2d3e8db
Danny <danny@cloudtengroup.co.uk>
2019-10-24 20:38:54 +0100

    release

---
+++ incogneo/background.js
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.extension.isAllowedIncognitoAccess(function(isAllowedAccess) {
+    if (isAllowedAccess) return; // Great, we've got access
+
+    // alert for a quick demonstration, please create your own user-friendly UI
+    alert('Please allow incognito mode in the following screen.');
+
+    chrome.tabs.create({
+      url: 'chrome://extensions/?id=' + chrome.runtime.id
+    });
+  });
+});
+chrome.tabs.onCreated.addListener(function(tab){
+  if( tab.incognito && tab.url == 'chrome://newtab/' ) {
+    chrome.tabs.update({url: chrome.extension.getURL("incogneo.html")});
+  }
+});
---
+++ incogneo/reeves.js
@@ -0,0 +1,2 @@
+var reeves = Math.floor(Math.random() * 5) + 1;
+document.querySelector('.icon').style.backgroundImage = 'url(reeves/k' + reeves + '.png)';
